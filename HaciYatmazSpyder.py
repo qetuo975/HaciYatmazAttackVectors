a66b05af6085051f5f947ddf6ad27e06a16cfb37
Can Erciyas <85997937+qetuo975@users.noreply.github.com>
2023-07-14 02:59:25 +0300

    Update HaciYatmazSpyder.py

--- HaciYatmazSpyder.py
+++ HaciYatmazSpyder.py
@@ -16,9 +16,9 @@ href_links = extract_links_from(target_url)
 for link in href_links:
 	link = urlparse.urljoin(target_url,link)
 
-	if "#" in link:	# #r refers to original page so avoid duplicate page again and again
+	if "#" in link:	
 		link = link.split("#")[0]
 
-	if target_url in link and link not in target_links: #to avoid repeating the same url
+	if target_url in link and link not in target_links: 
 		target_links.append(link)
 		print(link)
